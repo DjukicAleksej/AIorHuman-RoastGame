fd2551041fac903539c16b3795dd212d34bf9e37
DjukicAleksej <aleksejdjukicets@gmail.com>
2026-01-15 22:52:07 +0100

    Added web socket managament for when the user starts the game and I set the game phase to start , I then set the phase to chat and reset all the messages

--- frontend/src/App2.js
+++ frontend/src/App2.js
@@ -12,6 +12,20 @@ export default function App(){
 
     const [phase, setPhase] = useState("LOBBY"); // LOBBY, IN_GAME, GAME_OVER
     const [result,setResult] = useState(null); //WIN OR LOSE
-    
 
+    useEffect(() => {
+        const socket = new WebSocket(WS_URL);
+        socket.onopen = () => console.log("Connected to WS");
+        socket.onmessage = (msg) => {
+            const data = JSON.parse(msg.data);
+            if(data.type === "GAME_START"){
+                setGameId(data.gameId);
+                setMessages([]);
+                setPhase("CHAT");
+            }
+
+
+            
+        }
+    })
 }
