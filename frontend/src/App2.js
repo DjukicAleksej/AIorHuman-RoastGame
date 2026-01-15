c47f20628e1efb39032b681fe6bee5a98727c5c6
DjukicAleksej <aleksejdjukicets@gmail.com>
2026-01-15 22:47:09 +0100

    Added all data I will need for the frontend such as the roasts name etc

--- frontend/src/App2.js
+++ frontend/src/App2.js
@@ -4,5 +4,14 @@ import "./App.css";
 const WS_URL = "ws://localhost:5000";
 
 export default function App(){
+    const [ws, setWs] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState("");
+    const [name, setName] = useState("");
+    const [gameId, setGameId] = useState(null);
+
+    const [phase, setPhase] = useState("LOBBY"); // LOBBY, IN_GAME, GAME_OVER
+    const [result,setResult] = useState(null); //WIN OR LOSE
     
+
 }
